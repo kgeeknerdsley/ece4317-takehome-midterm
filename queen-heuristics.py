93b992c23ae547d52c1d4a41866cbe2bf38a9856
Kevin Worsley <kgeekmail@gmail.com>
2020-10-25 17:11:11 -0700

    Program complete. but columns are incorrect

--- queen-heuristics.py
+++ queen-heuristics.py
@@ -1,5 +1,7 @@
 from queen import Queen
 
+foundDuplicate = False
+
 board = [[0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,27 +22,69 @@ heuristicBoard = [[0, 0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-def printBoard():
-    for row in board:
+def printBoard(gameBoard):
+    for row in gameBoard:
         print(row)
 
 #master list to hold all queen objects
-queens = [Queen(4,0), Queen(5,1), Queen(6,2), Queen(3,3), Queen(4,4), Queen(5,5), Queen(6,6), Queen(5,7)]
+queens = [Queen(4,0,1), Queen(5,1,2), Queen(6,2,3), Queen(3,3,4), Queen(4,4,5), Queen(5,5,6), Queen(6,6,7), Queen(5,7,8)]
+verifiedHits = []
 
 #place all queens in the appropriate spot
 for i in range(len(queens)):
-    board[queens[i].getRow()][queens[i].getCol()] = 1
+    board[queens[i].getRow()][queens[i].getCol()] = queens[i].getNum()
 
-printBoard()
+printBoard(board)
 
-test = queens[3].findHitPairs(board)
-print(test)
-x = test[1]
-x = x[::-1]
-print(x)
+#get the hit list for each queen
+#compare each item of the queen hit list
+#if that hit, reversed, is not in the list, add the forwards version
 
+lastRPos = queens[0].originalRow
+lastCPos = queens[0].originalCol
 
+#to iterate over every square
+for column in range(8):
+    #reset last column's piece
+    if(column != 0):
+        board[7][column-1] = 0
+        board[queens[column-1].getOriginalRow()][queens[column-1].getOriginalCol()] = column
 
+    lastRPos = queens[column].originalRow #last known position of current column was original spot
+    lastCPos = queens[column].originalCol
+    
+    for row in range(8):
+        verifiedHits = []#reset verifiedHits for the new run
+        queens[column].setPosition(row,column) #queen object now knows their new spot
+        board[row][column] = column+1 #set the new piece in board
+        board[lastRPos][lastCPos] = 0 #unset the last piece's position
+        
+        #getSquareVal()...............................................................................
+        for j in range(len(queens)): #for the entire queen list, find the hits
+            queenHitList = []
+            queenHitList = queens[j].findHitPairs(board) #might need reinitialization
+
+            for hit in range(len(queenHitList)): #for all the hits, test them
+                foundDuplicate = False
+                forwardHit = queenHitList[hit]
+                reversedHit = forwardHit[::-1]
+
+                for confirmedHit in range(len(verifiedHits)):# for every hit already found, check it against new hit
+                    if(reversedHit == verifiedHits[confirmedHit]):
+                        foundDuplicate = True
+
+                if(not foundDuplicate):
+                    verifiedHits.append(forwardHit)
+        #..................................................................................................
+        heuristicBoard[row][column] = len(verifiedHits)
+
+        lastRPos = row
+        lastCPos = column
+
+print("\n")
+printBoard(board)
+print("\n")
+printBoard(heuristicBoard)
 
 
 #find the new spot
--- queen.py
+++ queen.py
@@ -1,13 +1,25 @@
 class Queen:
-    def __init__(self, r, c):
+    def __init__(self, r, c, n):
         self.rowPos = r
         self.colPos = c
+        self.number = n
+        self.originalRow = r
+        self.originalCol = c
 
     def getRow(self):
         return self.rowPos
 
     def getCol(self):
         return self.colPos
+
+    def getNum(self):
+        return self.number
+
+    def getOriginalRow(self):
+        return self.originalRow
+
+    def getOriginalCol(self):
+        return self.originalCol
 
     def setPosition(self, r, c):
         self.rowPos = r
@@ -25,45 +37,95 @@ class Queen:
 
         for i in range(8):
             #horizontal motion
-            if(board[self.rowPos][i] == 1):
-                print("Found a queen on the horizontal at position (" + str(self.rowPos) + "," + str(i) + ")\n")
+            if(board[self.rowPos][i] != 0):
+                #print("Found a queen on the horizontal at position (" + str(self.rowPos) + "," + str(i) + ")\n")
 
-                toAdd = tuple((self.rowPos,i))
+                toAdd = tuple((self.number,board[self.rowPos][i])) #tuple is the queen hitting, and queen getting hit
 
-                if(toAdd != (self.rowPos, self.colPos)):
+                if(toAdd != (self.number, self.number)):
                     possibleHits.append(toAdd)
 
             #vertical motion
-            if(board[i][self.colPos] == 1):
-                print("Found a queen on the vertical at position (" + str(i) + "," + str(self.colPos) + ")\n")
+            if(board[i][self.colPos] != 0):
+                #print("Found a queen on the vertical at position (" + str(i) + "," + str(self.colPos) + ")\n")
 
-                toAdd = tuple((i,self.colPos))
+                toAdd = tuple((self.number,board[i][self.colPos]))
 
-                if(toAdd != (self.rowPos, self.colPos)):
+                if(toAdd != (self.number, self.number)):
                     possibleHits.append(toAdd)
         
+        #yes, I know these diagonal moves are inefficient and bad
+
         #downward left diagonal
         diagR = self.rowPos
         diagC = self.colPos
         while(not downLeft): #downLeft goes true when we're done
-            if(board[diagR][diagC] == 1):
-                print("Found a queen on the down left diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the down left diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
 
-                toAdd = tuple((self.rowPos,i))
+                toAdd = tuple((self.number,board[diagR][diagC]))
 
-                if(toAdd != (self.rowPos, self.colPos)):
+                if(toAdd != (self.number, self.number)):
                     possibleHits.append(toAdd)
 
-            if(diagC == 0):
+            if(diagC == 0 or diagR == 7):
                 downLeft = True
             else:
                 diagR = diagR + 1
                 diagC = diagC - 1
-                
-            
 
-            
+        #downward right diagonal
+        diagR = self.rowPos
+        diagC = self.colPos
+        while(not downRight):
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the down right diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
 
+                toAdd = tuple((self.number,board[diagR][diagC]))
 
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+
+            if(diagC == 7 or diagR == 7):
+                downRight = True
+            else:
+                diagR = diagR + 1
+                diagC = diagC + 1
+
+        #upward left diagonal
+        diagR = self.rowPos
+        diagC = self.colPos
+        while(not upLeft):
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the upper left diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
+
+                toAdd = tuple((self.number,board[diagR][diagC]))
+
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+
+            if(diagC == 0 or diagR == 0):
+                upLeft = True
+            else:
+                diagR = diagR - 1
+                diagC = diagC - 1
+
+        #upward right diagonal
+        diagR = self.rowPos
+        diagC = self.colPos
+        while(not upRight):
+            if(board[diagR][diagC] != 0):
+                #print("Found a queen on the upper right diagonal at position (" + str(diagR) + "," + str(diagC) + ")\n")
+
+                toAdd = tuple((self.number,board[diagR][diagC]))
+
+                if(toAdd != (self.number, self.number)):
+                    possibleHits.append(toAdd)
+
+            if(diagC == 7 or diagR == 0):
+                upRight = True
+            else:
+                diagR = diagR - 1
+                diagC = diagC + 1
 
         return possibleHits
