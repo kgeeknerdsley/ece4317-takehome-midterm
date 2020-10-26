8c4853ccac1938d6aa5af868f224b35d9d257c5d
Kevin Worsley <kgeekmail@gmail.com>
2020-10-25 19:36:14 -0700

    Add pretty board

--- queen-heuristics.py
+++ queen-heuristics.py
@@ -1,4 +1,6 @@
 from queen import Queen
+import pygame
+from pygame.locals import *
 
 foundDuplicate = False
 
@@ -86,10 +88,52 @@ printBoard(board)
 print("\n")
 printBoard(heuristicBoard)
 
+#https://stackoverflow.com/questions/45945254/make-a-88-chessboard-in-pygame-with-python
 
-#find the new spot
-# set the appropriate queen's location
-# foreach through the list of queens, generating their list of hits
-# compare each list to the master hit list, eliminating duplicates
-# print final number to the spot in completedBoard
-# reset queen positions, empty lists, change spot
+pygame.init()
+S_WIDTH = 700
+S_LENGTH = 700
+
+screen = pygame.display.set_mode((S_WIDTH, S_LENGTH))
+pygame.display.set_caption("Chessboard")
+
+size = 70
+lb = (209, 196, 132)
+db = (122, 105, 18)
+black = (0,0,0)
+white= (255,255,255)
+
+#board length, must be even
+boardLength = 8
+screen.fill((255,255,255))
+
+text = pygame.font.Font(None, 36)
+queenImage = pygame.image.load('queen.png')
+
+cnt = 0
+for i in range(1,boardLength+1):
+    for z in range(1,boardLength+1):
+        #check if current loop value is even
+        if cnt % 2 == 0:
+            pygame.draw.rect(screen, lb,[size*z,size*i,size,size])
+        else:
+            pygame.draw.rect(screen, db, [size*z,size*i,size,size])
+
+        number = text.render(str(heuristicBoard[i-1][z-1]), True, black)
+        screen.blit(number,(size*z+5,size*i+5))
+
+        for queensToDraw in range(len(queens)):
+            qRow = queens[queensToDraw].getOriginalRow()
+            qCol = queens[queensToDraw].getOriginalCol()
+
+            if(i-1 == qRow and z-1 == qCol):
+                screen.blit(queenImage, (size*z+30,size*i+30))
+        cnt +=1
+    #since theres an even number of squares go back one value
+    #screen.blit(number,(size*i,size*z))
+    cnt-=1
+#Add a nice boarder
+pygame.draw.rect(screen,black,[size,size,boardLength*size,boardLength*size],1)
+
+while(1):
+    pygame.display.update()
